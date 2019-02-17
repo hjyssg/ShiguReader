db61598885ed39b9afd2d018c7fee9442138a778
aji47 <the398355565@gmail.com>
2019-02-17 15:04:24 +0800

    no copy-paster

--- src/client/subcomponent/FileChangeToolbar.js
+++ src/client/subcomponent/FileChangeToolbar.js
@@ -4,8 +4,8 @@ const spop  = require("./spop");
 const userConfig = require('../../user-config');
 import PropTypes from 'prop-types';
 var classNames = require('classnames');
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
 import Swal from 'sweetalert2';
 import Sender from '../Sender';
 
@@ -23,7 +23,6 @@ export default class FileChangeToolbar extends Component {
         Swal.fire({
             title: "Delete",
             text: 'Do you want to delete this file?' ,
-            type: 'warning',
             showCancelButton: true,
             confirmButtonText: 'Yes',
             cancelButtonText: 'No'
@@ -32,7 +31,7 @@ export default class FileChangeToolbar extends Component {
                 Sender.simplePost("/api/deleteFile", {src: this.props.file}, res => {
                     if (!res.failed) {
                         spop({
-                            template: 'Delete successfully',
+                            template: 'Delete ' + this.props.file + ' successfully',
                             position: 'bottom-center'
                         });
                     }else{
@@ -52,9 +51,7 @@ export default class FileChangeToolbar extends Component {
 
         if(typeof path === "string"){
             Swal.fire({
-                title: "Move File",
-                text: 'Do you want to move this file to ' + path +"?",
-                type: 'warning',
+                html: 'Do you want to move this file to <span class="path-highlight">'+  path +"</span>",
                 showCancelButton: true,
                 confirmButtonText: 'Yes',
                 cancelButtonText: 'No'
@@ -63,7 +60,7 @@ export default class FileChangeToolbar extends Component {
                     Sender.simplePost("/api/moveFile", {src: this.props.file, dest: path}, res => {
                         if (!res.failed) {
                             spop({
-                                template: 'Moved Successfully',
+                                template: ['Moved', this.props.file, "to", path, 'Successfully'].join(""),
                                 position: 'bottom-center'
                             });
                         }else{
@@ -79,48 +76,44 @@ export default class FileChangeToolbar extends Component {
         }
     };
 
-    copyToClipboard(path, mode){
-        //https://stackoverflow.com/questions/49236100/copy-text-from-span-to-clipboard
-        var textArea = document.createElement("textarea");
-        if(mode === "delete"){
-            textArea.value = "DEL /f \"" +  path + "\"";
-        } else if(mode === "good"){
-            textArea.value = "MOVE \"" +  path + "\"" + "  \"" + userConfig.good_folder +"\"";
-        }else if(mode === "not_good"){
-            textArea.value = "MOVE \"" +  path + "\"" + "   \"" + userConfig.not_good_folder +"\"";
-        }
+    // copyToClipboard(path, mode){
+    //     //https://stackoverflow.com/questions/49236100/copy-text-from-span-to-clipboard
+    //     var textArea = document.createElement("textarea");
+    //     if(mode === "delete"){
+    //         textArea.value = "DEL /f \"" +  path + "\"";
+    //     } else if(mode === "good"){
+    //         textArea.value = "MOVE \"" +  path + "\"" + "  \"" + userConfig.good_folder +"\"";
+    //     }else if(mode === "not_good"){
+    //         textArea.value = "MOVE \"" +  path + "\"" + "   \"" + userConfig.not_good_folder +"\"";
+    //     }
 
-        document.body.appendChild(textArea);
-        textArea.select();
-        document.execCommand("Copy");
-        textArea.remove();
+    //     document.body.appendChild(textArea);
+    //     textArea.select();
+    //     document.execCommand("Copy");
+    //     textArea.remove();
     
-        spop({
-          template: 'Copied to Clipboard',
-          position: 'bottom-right',
-          autoclose: 3000
-        });
-    }
+    //     spop({
+    //       template: 'Copied to Clipboard',
+    //       position: 'bottom-right',
+    //       autoclose: 3000
+    //     });
+    // }
 
-    renderDirectChange(){
-        const { anchorEl } = this.state;
-        const {file, className, showDirectChange} = this.props;
-        if(showDirectChange){
-            return (<div>
-                    <button onClick={this.handleClick} className="fas fa-arrow-right" title="directly move"/>
-                    <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={this.handleClose}
-                    >
-                        <MenuItem onClick={this.handleClose.bind(this, userConfig.good_folder)}>{"Move to " + userConfig.good_folder}</MenuItem>
-                        <MenuItem onClick={this.handleClose.bind(this, userConfig.not_good_folder)}>{"Move to " + userConfig.not_good_folder}</MenuItem>
-                        <MenuItem onClick={this.handleDelete.bind(this)}><i className="fas fa-trash-alt file-bar-delete-button">Delete</i></MenuItem>
-                    </Menu>
-            </div>);
-        }
-    }
+    // renderDirectChange(){
+    //     const { anchorEl } = this.state;
+    //         return (<div>
+    //                 <button onClick={this.handleClick} className="fas fa-arrow-right" title="directly move"/>
+    //                 <Menu
+    //                 id="simple-menu"
+    //                 anchorEl={anchorEl}
+    //                 open={Boolean(anchorEl)}
+    //                 onClose={this.handleClose}
+    //                 >
+    //                     <MenuItem onClick={this.handleClose.bind(this, userConfig.good_folder)}>{"Move to " + userConfig.good_folder}</MenuItem>
+    //                     <MenuItem onClick={this.handleClose.bind(this, userConfig.not_good_folder)}>{"Move to " + userConfig.not_good_folder}</MenuItem>
+    //                 </Menu>
+    //         </div>);
+    // }
 
     render(){
         const {file, className, showDirectChange} = this.props;
@@ -130,22 +123,18 @@ export default class FileChangeToolbar extends Component {
             <div className={cn} >
                 <div className="explorer-delete-cmd fas fa-trash-alt"
                                 title="Copy Del"
-                                onClick={this.copyToClipboard.bind(this, file, "delete")}></div>
+                                onClick={this.handleDelete.bind(this)}></div>
                 <div className="explorer-delete-cmd fas fa-check"
                                 title={"Move to " + userConfig.good_folder}
-                                onClick={this.copyToClipboard.bind(this, file, "good")}></div>
+                                onClick={this.handleClose.bind(this, userConfig.good_folder)}></div>
                 <div className="explorer-delete-cmd fas fa-times"
                                 title={"Move to " + userConfig.not_good_folder}
-                                onClick={this.copyToClipboard.bind(this, file, "not_good")}></div>
-                {this.renderDirectChange()}
-        </div>
+                                onClick={this.handleClose.bind(this, userConfig.not_good_folder)}></div>
+            </div>
         )
      }
 }
 
-
-
 FileChangeToolbar.propTypes = {
-    file: PropTypes.string,
-    showDirectChange: PropTypes.bool
+    file: PropTypes.string
 };
