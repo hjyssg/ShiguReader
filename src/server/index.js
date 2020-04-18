567bf9caa44cb5f674f2f7becb40251050065c2e
aji47 <the398355565@gmail.com>
2020-04-18 09:14:28 +0800

    use json db and set maxage to allow browser cache

--- src/server/index.js
+++ src/server/index.js
@@ -32,7 +32,14 @@ logPath = path.join(logPath, dateFormat(new Date(), "isoDate"))+ ".log";
 let file_db_path =  path.join(__dirname, "..", "..",  userConfig.workspace_name, "shigureader_local_file_info");
 const sevenZipPath = path.join(process.cwd(), "resource/7zip");
 
-console.log("process.argv", process.argv);
+
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+let zip_content_db_path =  path.join(__dirname, "..", "..",  userConfig.workspace_name, "shigureader_zip_file_content_info");
+
+const zip_content_db = new JsonDB(new Config(zip_content_db_path, true, true, '/'));
+
+// console.log("process.argv", process.argv);
 const isProduction = process.argv.includes("--production");
 
 console.log("--------------------");
@@ -83,12 +90,12 @@ const db = {
     cacheTable: {},
     //hash to any string
     hashTable: {},
-    //has no thumbnail file
-    hasNoThumbnail: {}
 };
 
 app.use(express.static('dist'));
-app.use(express.static(root));
+app.use(express.static(root, {
+    maxAge: '12096000000' // uses milliseconds per docs
+}));
 
 //  to consume json request body
 //  https://stackoverflow.com/questions/10005939/how-do-i-consume-the-json-post-data-in-an-express-application
@@ -155,7 +162,7 @@ async function init() {
 
     console.log("scanning local files");
 
-    const filter = (e) => {return isCompress(e) || isImage(e);};
+    const filter = (e) => {return isCompress(e);};
     let beg = (new Date).getTime()
     const results = fileiterator(userConfig.home_pathes, { 
         filter:filter, 
@@ -594,62 +601,77 @@ function chooseOneZip(files){
     return tempFiles[0];
 }
 
+
 async function getFirstImageFromZip(fileName, res, mode, counter) {
-    if(!util.isCompress(fileName) || db.hasNoThumbnail[fileName]){
+    if(!util.isCompress(fileName)){
         return;
     }
 
-    const outputPath = getOutputPath(fileName);
-    const temp = getCache(outputPath);
-    const isPreG = mode === "pre-generate";
-    if(isPreG){
-        res.send = () => {};
-        res.sendStatus = () => {};
-        res.sendFile = () => {};
-    }
+    const isPregenerateMode = mode === "pre-generate";
+    const sendable = !isPregenerateMode;
 
-    function sendImage(img){
-        let ext = path.extname(img);
-        ext = ext.slice(1);
-        res.setHeader('Content-Type', 'image/' + ext );
-        res.sendFile(path.resolve(img));
-    }
-
-    if (temp && temp.files) {
-        const img = chooseOneImage(temp.files);
-        if(img){
-            sendImage(img);
+    const contentInfo = zip_content_db.getData("/");
+    if(contentInfo[fileName]){
+        if(contentInfo[fileName].thumbnail) {
+            const img = contentInfo[fileName].thumbnail;
+            if(img){
+                sendImage(img);
+                return;
+            }
+        }else if(!contentInfo[fileName].thumbnail){
             return;
         }
     }
+ 
+    const outputPath = getOutputPath(fileName);
+ 
+    function sendImage(img){
+        let ext = path.extname(img);
+        ext = ext.slice(1);
+        sendable && res.setHeader('Content-Type', 'image/' + ext );
+        sendable && res.sendFile(path.resolve(img));
+    }
 
     const stats = await pfs.stat(fileName);
-
     const fileSizeInBytes = stats["size"]
     //Convert the file size to megabytes (optional)
     const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
 
+    function updateZipDb(thumbnail, pageNum){
+        // const time1 = new Date().getTime();
+        contentInfo[fileName] = {
+            thumbnail: thumbnail,
+            pageNum: pageNum
+        };
+        // zip_content_db.push("/", contentInfo);
+        // const time2 = new Date().getTime();
+        // console.log("[jsondb push]", (time2-time1)/1000);
+    }
+
     try{
         //bigger than 30mb
-        if(fileSizeInMegabytes > userConfig.full_extract_for_thumbnail_size || isPreG){
+        if(fileSizeInMegabytes > userConfig.full_extract_for_thumbnail_size || isPregenerateMode){
             // assume zip
             // let {stdout, stderr} = await limit(() => execa(sevenZip, ['l', '-ba', fileName]));
             //https://superuser.com/questions/1020232/list-zip-files-contents-using-7zip-command-line-with-non-verbose-machine-friend
             let {stdout, stderr} = await limit(() => execa(sevenZip, ['l', '-r', '-ba' ,'-slt', fileName]));
             const text = stdout;
+            
+
             if (!text) {
                 console.error("[getFirstImageFromZip]", "no text");
-                res.send("404 fail");
+                sendable && res.send("404 fail");
+                updateZipDb(null, 0);
                 return;
             }
-            
+
             const files = read7zOutput(text);
             const one = chooseOneImage(files);
-
+            
             if (!one) {
                 console.error("[getFirstImageFromZip]", fileName,  "no files from output");
-                db.hasNoThumbnail[fileName] = true;
-                res.sendStatus(404);
+                sendable && res.sendStatus(404);
+                updateZipDb(null, 0);
                 return;
             }
 
@@ -662,14 +684,15 @@ async function getFirstImageFromZip(fileName, res, mode, counter) {
                 let temp = path.join(outputPath, one);
                 temp = cleanFileName(temp);
                 sendImage(temp);
+                updateZipDb(temp, files.length);
 
-                if(isPreG){
+                if(isPregenerateMode){
                     counter.counter++;
                     console.log("[getFirstImageFromZip] pre-generate", counter.counter, "/", counter.total);
                 }
             } else {
                 console.error("[getFirstImageFromZip extract exec failed]", code);
-                res.sendStatus(404);
+                sendable && res.sendStatus(404);
             }
         } else {
             (async () => {
@@ -682,19 +705,19 @@ async function getFirstImageFromZip(fileName, res, mode, counter) {
                         if (img) {
                             sendImage(img);
                         } else {
-                            res.sendStatus(404);
+                            sendable && res.sendStatus(404);
                             console.error('[getFirstImageFromZip extract] no image output') 
                         }
                     });
                 } else {
-                    res.sendStatus(404);
+                    sendable && res.sendStatus(404);
                     console.error('[getFirstImageFromZip extract] exit: ', stderr);
                 }
             })();
         }
     } catch(e) {
         console.error("[getFirstImageFromZip] exception", e);
-        res.sendStatus(404);
+        sendable && res.sendStatus(404);
     }
 }
 
