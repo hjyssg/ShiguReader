046f8c5d930f52ed8818eabd89c42b00edc3da58
aji47 <the398355565@gmail.com>
2020-05-03 11:08:54 +0800

    iosCopyToClipboard

--- src/client/subcomponent/ClickAndCopyText.js
+++ src/client/subcomponent/ClickAndCopyText.js
@@ -4,6 +4,30 @@ import PropTypes from 'prop-types';
 var classNames = require('classnames');
 const spop  = require("./spop");
 const util = require("../../util");
+const clientUtil = require("../clientUtil");
+
+
+function iosCopyToClipboard(el) {
+  //https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios/34046084
+  var oldContentEditable = el.contentEditable,
+      oldReadOnly = el.readOnly,
+      range = document.createRange();
+
+  el.contentEditable = true;
+  el.readOnly = false;
+  range.selectNodeContents(el);
+
+  var s = window.getSelection();
+  s.removeAllRanges();
+  s.addRange(range);
+
+  el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+  el.contentEditable = oldContentEditable;
+  el.readOnly = oldReadOnly;
+
+  document.execCommand('copy');
+}
 
 export default class ClickAndCopyText extends Component {
   onTitleClick(){
@@ -11,16 +35,21 @@ export default class ClickAndCopyText extends Component {
     var textArea = document.createElement("textarea");
     textArea.value = this.props.text;
     document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("Copy");
+
+    if(clientUtil.isPad()){
+      iosCopyToClipboard(textArea)
+    }else{
+      textArea.select();
+      document.execCommand("Copy");
+    }
     textArea.remove();
-   
     spop({
       template: 'Copied to Clipboard',
       position: 'bottom-right',
       autoclose: 3000
     });
   }
+
 
   render(){
     const { text, className, ...others } = this.props;
