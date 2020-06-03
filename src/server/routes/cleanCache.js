c2613e9762ab42c13cb875df2dd7055fbe7dc3d7
aji47 <the398355565@gmail.com>
2020-06-03 11:10:51 +0800

    clean cache during init
    remove file watch on thunbnail

--- src/server/routes/cleanCache.js
+++ src/server/routes/cleanCache.js
@@ -2,12 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const serverUtil = require("../serverUtil");
-const db = require("../models/db");
-const { getAllFilePathes } = db;;
-const path = require('path');
-const getCacheOutputPath = db.getCacheOutputPath;
 const cachePath = serverUtil.common.cachePath;
-
 const pathUtil = require("../pathUtil");
 
 const deleteEmpty = require('delete-empty');
@@ -22,19 +17,11 @@ function doCacheClean(config){
 }
 
 router.post('/api/cleanCache', (req, res) => {
-    const minized = req.body && req.body.minized;
-
-    const allowFileNames =  getAllFilePathes().map(filePath => {
-        let outputPath = getCacheOutputPath(cachePath, filePath);
-        outputPath = path.basename(outputPath);
-        return outputPath;
-    })
-
     function afterClean() {
         res.sendStatus(200);
     }
 
-    doCacheClean({minized: minized, allowFileNames: allowFileNames, afterClean: afterClean});
+    doCacheClean({afterClean: afterClean});
 
     //delete empty folder
     deleteEmpty(pathUtil.getImgConverterCachePath(), (err, deleted) => {
