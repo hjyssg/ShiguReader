f934f6d10c9d0fd377b33254d91f5a605af1a674
aji47 <the398355565@gmail.com>
2020-11-30 01:26:46 +0800

    better language

--- src/client/ErrorPage.js
+++ src/client/ErrorPage.js
@@ -10,15 +10,15 @@ export default class ErrorPage extends Component {
         let text;
 
         if(status === 504){
-            text = "Is the server running right now?";
-        }  else if(status === 404 && filePath){
-            text = `Could not find ${filePath}.`;
-        } else if (status === 500 && filePath){
-            text = `${filePath} is a broken file`;
-        } else if(res.isFailed()){
+            text = "The backend server did not start.";
+        } else if(res.isFailed() && res.json.reason){
             status = "ERROR"
             statusText = res.json.reason;
             text = filePath;
+        }   else if(status === 404 && filePath){
+            text = `Could not find ${filePath}.`;
+        } else if (status === 500 && filePath){
+            text = `${filePath} is a broken file`;
         }
 
         return (
@@ -28,9 +28,10 @@ export default class ErrorPage extends Component {
                     {status}
                 </div>
                 <h2>{statusText}</h2>
+                <h2 >{text}</h2>
                 {/* <a href="#">Go To Homepage</a> */}
             </div>
-            <div className="error-user-text">{text}</div>
+           
         </div>);
      }
 }
