02ce5f4642b9515543ff4f77befe3ea9637eaecd
hjy <the398355565@gmail.com>
2020-12-01 18:09:57 +0800

    test

--- auto_test/test.js
+++ auto_test/test.js
@@ -2,6 +2,8 @@ const puppeteer = require('puppeteer');
 // https://stackoverflow.com/questions/25678063/whats-the-difference-between-assertion-library-testing-framework-and-testing-e#:~:text=Assertion%20libraries%20are%20tools%20to,do%20thousands%20of%20if%20statements.
 const { expect }  = require('chai');
 
+const prefix_s = "test_screenshot/";
+
 
 describe('ShiguReader Render Testing', function(){
 
@@ -23,7 +25,7 @@ describe('ShiguReader Render Testing', function(){
 
     after(async function() {
         // runs once after the last test in this block
-        // await browser.close();
+        await browser.close();
     });
 
     beforeEach(async function() {
@@ -31,38 +33,48 @@ describe('ShiguReader Render Testing', function(){
     });
 
     afterEach(async function() {
-    //    await page.close();
+       await page.close();
     });
 
     it('home', async function() {
-        await page.goto("http://localhost:3000/")
+        await page.goto("http://localhost:3000/");
         const title = await page.title();
         expect(title).to.eql('ShiguReader');
+        await page.screenshot({path: prefix_s +'home.png'});
     });
 
 
     it('tag page', async function() {
-        await page.goto("http://localhost:3000/tagPage/")
+        await page.goto("http://localhost:3000/tagPage/");
+        await page.screenshot({path: prefix_s + 'tag page.png'});
+
+
         // const title = await page.title();
         // expect(title).to.eql('ShiguReader');
     });
 
     it('authorPage', async function() {
-        await page.goto("http://localhost:3000/authorPage/")
+        await page.goto("http://localhost:3000/authorPage/");
+        await page.screenshot({path: prefix_s + 'authorPage.png'});
+
         // const title = await page.title();
         // expect(title).to.eql('ShiguReader');
     });
 
 
     it('chart', async function() {
-        await page.goto("http://localhost:3000/chart")
+        await page.goto("http://localhost:3000/chart");
+        await page.screenshot({path: prefix_s + 'chart.png'});
+
         // const title = await page.title();
         // expect(title).to.eql('ShiguReader');
     });
 
 
     it('admin', async function() {
-        await page.goto("http://localhost:3000/admin")
+        await page.goto("http://localhost:3000/admin");
+        await page.screenshot({path: prefix_s + 'admin.png'});
+
         // const title = await page.title();
         // expect(title).to.eql('ShiguReader');
     })
