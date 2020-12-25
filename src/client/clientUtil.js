2c3a9ac852582237440da868481c935ce8a5f588
aji47 <the398355565@gmail.com>
2020-12-26 00:14:07 +0800

    code review

--- src/client/clientUtil.js
+++ src/client/clientUtil.js
@@ -261,7 +261,7 @@ module.exports.CopyToClipboard = function(text){
 }
 
 //https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
-function download(filename, text) {
+module.exports.download = function(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
