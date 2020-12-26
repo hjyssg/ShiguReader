66a9164ef326fe947239072351eaa6bf1b79f708
aji47 <the398355565@gmail.com>
2020-12-26 11:26:12 +0800

    ut fixing

--- src/test/picker.test.js
+++ src/test/picker.test.js
@@ -7,13 +7,12 @@ describe('name picker', () => {
     it('normal', ()=> {
 
         s1 = null;
-        result = picker.parse(s1);
+        result = picker.pick(s1);
         assert.ok(result === null);
 
         s1 = "[LCBD-00601] Riho Iida 飯田里穂 – りほの島、楽園の光 Blu-ray";
-        result = parser.parse(s1);
-        console.log(result)
-        assert.ok(result === null);
+        result = picker.pick(s1);
+        assert.deepEqual(result,  ["飯田里穂"]);
     });
 
 });
