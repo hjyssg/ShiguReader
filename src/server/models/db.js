2ae0163d52660294540b057a557585f2c073e510
aji47 <the398355565@gmail.com>
2021-01-09 15:21:45 +0800

    CREATE INDEX IF NOT EXISTS

--- src/server/models/db.js
+++ src/server/models/db.js
@@ -58,9 +58,9 @@ function insertToTagTable(filePath, tag, type, subtype){
 }
 
 module.exports.createSqlIndex = function(){
-    sqlDb.run("CREATE INDEX filePath_index ON file_table (filePath)");
-    sqlDb.run("CREATE INDEX dirPath_index ON file_table (dirPath)");
-    sqlDb.run("CREATE INDEX tag_index ON tag_table (tag)");
+    sqlDb.run("CREATE INDEX IF NOT EXISTS filePath_index ON file_table (filePath)");
+    sqlDb.run("CREATE INDEX IF NOT EXISTS dirPath_index ON file_table (dirPath)");
+    sqlDb.run("CREATE INDEX IF NOT EXISTS tag_index ON tag_table (tag)");
 }
 
 const updateFileDb = function (filePath, statObj) {
