7084f9b2098b6ebb4ce6b83206674728862ab28d
aji47 <the398355565@gmail.com>
2021-01-18 00:40:21 +0800

    todo

--- src/common/sortUtil.js
+++ src/common/sortUtil.js
@@ -1,11 +1,19 @@
 const nameParser = require('../name-parser');
 const _ = require('underscore');
 
+//有三种时间
+// mtime: OS磁盘文件系统保存
+// rtime:最近一次的阅读时间
+// tag time:根据文件名推算出来的时间
 module.exports.sort_file_by_time = function (files, config) {
     const { fileInfos, getBaseName, fromEarly, onlyMtime, fileNameToReadTime } = config;
     return _.sortBy(files, a => {
         const fn = getBaseName(a);
         let fileTimeA;
+
+        //todo: only be mtime, or mtime + rtime
+
+
 
         if(fileNameToReadTime[fn]){
             fileTimeA = parseInt(fileNameToReadTime[fn]);
