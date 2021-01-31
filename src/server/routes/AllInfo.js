0d779aec54c53cad7771a0e243afae91ee2dd73c
aji47 <the398355565@gmail.com>
2021-01-31 15:05:26 +0800

    code review

--- src/server/routes/AllInfo.js
+++ src/server/routes/AllInfo.js
@@ -12,28 +12,29 @@ router.post('/api/tagInfo', async (req, res) => {
     const sqldb = db.getSQLDB();
 
     //inner joiner then group by
-    let sql = `SELECT a.filePath, max(a.sTime) as maxTime , b.tag, COUNT(b.tag) as count, b.type, b.subtype ` 
-    + `FROM (SELECT * FROM tag_table WHERE type = 'author' ) AS b LEFT JOIN `
-    + `(SELECT * FROM file_table where isCompress = true ) AS a `
-    + `ON a.filePath = b.filePath GROUP BY tag HAVING a.sTime = maxTime AND count > 1 ORDER BY count DESC`;
+    let sql = `SELECT a.filePath, max(a.sTime) as maxTime , b.tag, COUNT(b.tag) as count, b.type, b.subtype `
+        + `FROM (SELECT * FROM tag_table WHERE type = 'author' ) AS b LEFT JOIN `
+        + `(SELECT * FROM file_table where isCompress = true ) AS a `
+        + `ON a.filePath = b.filePath GROUP BY tag HAVING a.sTime = maxTime AND count > 1 ORDER BY count DESC`;
 
     //todo: sort by  a.sTime DESC
     let author_rows = await sqldb.allSync(sql);
- 
-    sql = `SELECT a.filePath, max(a.sTime) as maxTime , b.tag, COUNT(b.tag) as count, b.type, b.subtype ` 
-    + `FROM (SELECT * FROM tag_table WHERE type = 'tag') AS b LEFT JOIN `
-    + `(SELECT * FROM file_table where isCompress = true) AS a `
-    + `ON a.filePath = b.filePath GROUP BY tag HAVING a.sTime = maxTime AND count > 1 ORDER BY count DESC`;
+
+    sql = `SELECT a.filePath, max(a.sTime) as maxTime , b.tag, COUNT(b.tag) as count, b.type, b.subtype `
+        + `FROM (SELECT * FROM tag_table WHERE type = 'tag') AS b LEFT JOIN `
+        + `(SELECT * FROM file_table where isCompress = true) AS a `
+        + `ON a.filePath = b.filePath GROUP BY tag HAVING a.sTime = maxTime AND count > 1 ORDER BY count DESC`;
     let tag_rows = await sqldb.allSync(sql);
     const allRows = [].concat(author_rows, tag_rows);
 
-    for(let ii = 0; ii <  allRows.length; ii++){
-        const row =  allRows[ii];
-        row.thumbnail = await getThumbnailsForZip(row.filePath)
+    const thumbnails = await getThumbnailsForZip(allRows.map(e => e.filePath))
+    for (let ii = 0; ii < allRows.length; ii++) {
+        const row = allRows[ii];
+        row.thumbnail = thumbnails[row.filePath];
     }
 
     res.send({
-        author_rows, 
+        author_rows,
         tag_rows
     });
 });
