8c8ee64f3996d5f2d3b361d02a2d8baa63a289d2
aji47 <the398355565@gmail.com>
2021-02-05 12:30:35 +0800

    ItemsContainer hooks

--- src/client/subcomponent/ItemsContainer.js
+++ src/client/subcomponent/ItemsContainer.js
@@ -1,44 +1,28 @@
 
 const util = require("@common/util");
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 var classNames = require('classnames');
 
-export default class ItemsContainer extends Component {
-    static defaultProps = {
+export default function ItemsContainer(props) {
+    const [ open, setOpen ] = useState(false);
+    const { className, items, neverCollapse } = props;
+    const TOO_MUCH = 15;
 
-    }
-
-    state = {
-        open: false
-    };
-
-    toggleState() {
-        this.setState({
-            open: !this.state.open
-        })
-    }
-
-    render() {
-        const { className, items, neverCollapse } = this.props;
-        const { open } = this.state;
-        const TOO_MUCH = 15;
-
-        if (neverCollapse || items.length <= TOO_MUCH) {
-            return (
-                <ul className={classNames("dir-list container", className)}>
-                    {items}
-                </ul>);
-        } else {
-            const _items = open ? items : items.slice(0, TOO_MUCH);
-            const cn = classNames("item-container-expand-button", className, {
-                "fas fa-arrow-down": !open,
-                "fas fa-arrow-up": open,
-            });
-            return (
-                <ul className={"dir-list container"}>
-                    {_items}
-                    <div className={cn} onClick={this.toggleState.bind(this)} > </div>
-                </ul>);
-        }
+    if (neverCollapse || items.length <= TOO_MUCH) {
+        return (
+            <ul className={classNames("dir-list container", className)}>
+                {items}
+            </ul>);
+    } else {
+        const _items = open ? items : items.slice(0, TOO_MUCH);
+        const cn = classNames("item-container-expand-button", className, {
+            "fas fa-arrow-down": !open,
+            "fas fa-arrow-up": open,
+        });
+        return (
+            <ul className={"dir-list container"}>
+                {_items}
+                <div className={cn} onClick={() => { setOpen(!open) }} > </div>
+            </ul>);
     }
 }
