ab8677f509eea6e7fa47738ca1f0d0897bc4ac82
aji47 <the398355565@gmail.com>
2021-02-09 14:32:23 +0800

    regex

--- src/name-parser/all_in_one/index.js
+++ src/name-parser/all_in_one/index.js
@@ -1,13 +1,13 @@
-console.assert = console.assert || (() => {});
+console.assert = console.assert || (() => { });
 
 //--------------------------
 
 const same_tag_regs_table = {
-    "東方Project":[/^東方$/,/Touhou\s*Project/, /東方project/],
+    "東方Project": [/^東方$/, /Touhou\s*Project/, /東方project/],
     "オリジナル": [/^Original$/],
     "Kanon": [/カノン|Kanon/],
     "艦これ": [/艦これ|舰これ/, /艦隊これくしょん/, /Kantai\s*Collection/, /KanColle/],
-    "ラブライブ!" : [/Love Live/, /ラブライブ/],
+    "ラブライブ!": [/Love Live/, /ラブライブ/],
     "ラブライブ!サンシャイン!!": [/ラブライブ.*サンシャイン.*/],
     "プリンセスコネクト!Re:Dive": [/プリンセスコネクト.*Re.*Dive/],
     "Fate／Grand Order": [/Fate.*Grand.*Order/, /FGO/],
@@ -33,7 +33,7 @@ const same_tag_regs_table = {
     "D.C～ダ・カーポ": [/D\.C\./, /ダ.*カーポ/],
     "Dog Days": [/Dog.*Days.*/],
     "Dream C Club": [/Dream.*C.*Club/],
-    "ガンダム ":[/ガンダム|gundam/],
+    "ガンダム ": [/ガンダム|gundam/],
     "コードギアス": [/コードギアス/],
     "ご注文はうさぎですか": [/ご注文はうさぎですか/],
     "ソードアート・オンライン": [/ソードアート.*オンライン/, /Sword\s*Art\s*Online/],
@@ -48,7 +48,7 @@ const same_tag_regs_table = {
     "キング・オブ・ファイター": [/キング\.オブ\.ファイター/, /^KOF$/],
     "ファイアーエムブレム": [/ファイアーエムブレム/],
     "ファンタシースター": [/ファンタシースター|PhantasyStar/],
-    "To Heart 2":[/To.*Heart.*2|トゥハート2/]
+    "To Heart 2": [/To.*Heart.*2|トゥハート2/]
 }
 
 const not_author_but_tag = [
@@ -301,15 +301,15 @@ const char_names = [
     "Nelson",
     "Gotland",
     "Maestrale",
-    
+
     "穂乃果",
     "ことり",
-    
+
     "エルフ"
 ].filter(e => e.length > 1);
 //https://stackoverflow.com/questions/5582574/how-to-check-if-a-string-contains-text-from-an-array-of-substrings-in-javascript
 const char_name_regex = new RegExp(char_names.join("|"));
-const not_author_but_tag_regex =  new RegExp(not_author_but_tag.join("|"), "i");
+const not_author_but_tag_regex = new RegExp(not_author_but_tag.join("$|") + "$", "i");
 
 const book_types = [
     "同人音声",
@@ -327,19 +327,19 @@ const book_types = [
 ];
 const book_type_regex = new RegExp(book_types.map(e => `(${e})`).join("|"), "i");
 
-function isBookType(str){
+function isBookType(str) {
     return !!str.match(book_type_regex);
 }
 
-function getBookType(str){
+function getBookType(str) {
     return str.match(book_type_regex)[0];
 }
 
 const same_tag_matrix = [];
-for(let tag in same_tag_regs_table){
-    if(same_tag_regs_table.hasOwnProperty(tag)){
-        const big_pre_join = same_tag_regs_table[tag].map(e =>e.source)
-        const r =  new RegExp(big_pre_join.join("|"), 'i')
+for (let tag in same_tag_regs_table) {
+    if (same_tag_regs_table.hasOwnProperty(tag)) {
+        const big_pre_join = same_tag_regs_table[tag].map(e => e.source)
+        const r = new RegExp(big_pre_join.join("|"), 'i')
         const row = [r, tag];
         same_tag_matrix.push(row);
     }
@@ -359,28 +359,28 @@ const reitaisai_reg = /^例大祭.*\d+/;
 const tora_reg = /^とら祭り.*\d+/;
 const komitore_reg = /^こみトレ.*\d+/;
 const reg_list = [comicket_reg, comic_star_reg, love_live_event_reg,
-                 comitea_reg, sankuri_reg, reitaisai_reg,
-                 tora_reg, komitore_reg, /みみけっと.*\d+/, 
-                 /コミトレ.*\d+/, /FF\d+/, /iDOL SURVIVAL.*\d/i, 
-                 /SC\d+/, /コミコミ.*\d/, /ふたけっと.*\d/,
-                /ファータグランデ騎空祭/, /歌姫庭園/, /紅楼夢/, 
-                /CSP\d/, /CC大阪\d/, /COMITIA\d/];
+    comitea_reg, sankuri_reg, reitaisai_reg,
+    tora_reg, komitore_reg, /みみけっと.*\d+/,
+    /コミトレ.*\d+/, /FF\d+/, /iDOL SURVIVAL.*\d/i,
+    /SC\d+/, /コミコミ.*\d/, /ふたけっと.*\d/,
+    /ファータグランデ騎空祭/, /歌姫庭園/, /紅楼夢/,
+    /CSP\d/, /CC大阪\d/, /COMITIA\d/];
 
 const event_reg = new RegExp(reg_list.map(e => e.source).join("|"), "i");
 
-function belongToEvent(e){
+function belongToEvent(e) {
     return e.match(event_reg);
 }
 
 
 const comiket_to_date_table = {};
-function getDateFromParse(str){
+function getDateFromParse(str) {
     const pp = parse(str);
     let result;
-    if(pp){
-        if(pp.dateTag){
+    if (pp) {
+        if (pp.dateTag) {
             result = getDateFromStr(pp.dateTag);
-        }else if(pp.comiket){
+        } else if (pp.comiket) {
             result = getDateFromComiket(pp.comiket)
         }
     }
@@ -388,53 +388,53 @@ function getDateFromParse(str){
 }
 
 //for sort algo, not very accurate
-function getDateFromComiket(comiket){
+function getDateFromComiket(comiket) {
     let result = null;
     let num;
     let year;
     let month;
-    
+
     if (comiket_to_date_table[comiket]) {
         result = comiket_to_date_table[comiket];
-    } else if(comiket.match(comicket_reg)) {
+    } else if (comiket.match(comicket_reg)) {
         comiket = comiket.replace("C", "");
         num = parseInt(comiket);
-        year = Math.floor(num /2) + 1971;
+        year = Math.floor(num / 2) + 1971;
         const isSummer = num % 2 === 0;
-        month = isSummer? 8 : 11;
-        const day = isSummer? 10: 28;
+        month = isSummer ? 8 : 11;
+        const day = isSummer ? 10 : 28;
         result = new Date(year, month, day);
-    } else if(comiket.match(comic_star_reg)) {
+    } else if (comiket.match(comic_star_reg)) {
         comiket = comiket.replace("COMIC1☆", "");
         num = parseInt(comiket);
-        if(num <= 10){
+        if (num <= 10) {
             //once per year
-            result = new Date(2006+num, 3, 30);
-        }else{
+            result = new Date(2006 + num, 3, 30);
+        } else {
             num = (num - 10)
-            year = 2017 + Math.floor(num /2);
-            month = num % 2 === 0? 10 : 4;
+            year = 2017 + Math.floor(num / 2);
+            month = num % 2 === 0 ? 10 : 4;
             result = new Date(year, month, 30);
         }
     }
 
-    if(result){
+    if (result) {
         comiket_to_date_table[comiket] = result;
     }
     return result;
 }
 
-function getDateFromStr(str){
-    const mresult =  str.match(date_reg);
-    if(mresult){
+function getDateFromStr(str) {
+    const mresult = str.match(date_reg);
+    if (mresult) {
         let [wm, y, m, d] = mresult.filter(e => !!e);
         y = convertYearString(y);
-        m = parseInt(m)-1;
-        d = parseInt(d)||1;
+        m = parseInt(m) - 1;
+        d = parseInt(d) || 1;
 
-        if(m < 0 || m > 11){
+        if (m < 0 || m > 11) {
             return undefined;
-        }else if(d < 1 || d > 31){
+        } else if (d < 1 || d > 31) {
             return undefined;
         }
 
@@ -443,15 +443,15 @@ function getDateFromStr(str){
 }
 
 function convertYearString(y) {
-    if(y.length === 2){
-        y =  parseInt(y);
+    if (y.length === 2) {
+        y = parseInt(y);
         if (y > 70) {
             y = 1900 + y;
-        }else {
+        } else {
             y = 2000 + y;
         }
-    }else{
-        y =  parseInt(y)
+    } else {
+        y = parseInt(y)
     }
 
     return y;
@@ -462,7 +462,7 @@ function isDateValid(date) {
     // An invalid date object returns NaN for getTime() and NaN is the only
     // object not strictly equal to itself.
     return date.getTime() === date.getTime();
-};  
+};
 
 const dreg1 = /(\d{2})(\d{2})(\d{2})/;
 const dreg2 = /(\d{2})-(\d{2})-(\d{2})/;
@@ -472,50 +472,50 @@ const dreg5 = /(\d{4})年(\d{1,2})月(\d{1,2})日/;
 const dreg6 = /(\d{4})\.(\d{1,2})\.(\d{1,2})/;
 const date_reg = new RegExp([dreg1, dreg2, dreg3, dreg4, dreg5, dreg6].map(e => e.source).join("|"), "i");
 function isStrDate(str) {
-    if(str.match(date_reg)){
+    if (str.match(date_reg)) {
         const dd = getDateFromStr(str);
         return !!(dd && isDateValid(dd));
     }
 }
 
 
-function getGroupAndName(str){
+function getGroupAndName(str) {
     var macthes = str.match(/^(.*?)\s*\((.*?)\)$/);
-    if(macthes && macthes.length > 0){
+    if (macthes && macthes.length > 0) {
         return {
             group: macthes[1].trim(),
             name: macthes[2].trim(),
         };
-    }else{
+    } else {
         return {
             name: str.trim(),
         };
     }
 }
 
-function match(reg, str){
+function match(reg, str) {
     const result = [];
     var token = reg.exec(str);
-    while (token){
+    while (token) {
         result.push(token[1]);
         token = reg.exec(str);
     }
     return result;
 }
 
-function isNotAuthor(str){
+function isNotAuthor(str) {
     return str.match(not_author_but_tag_regex);
 }
 
 const useless_tag = /RJ\d+|DL版|別スキャン^エロ|^digital$|^\d+p|^\d+$/i;
-function isUselessTag(str){
+function isUselessTag(str) {
     return !!str.match(useless_tag)
 }
 
-function findMaxStr(arr){
+function findMaxStr(arr) {
     let res = arr[0];
     arr.forEach(e => {
-        if(e.length > res.length){
+        if (e.length > res.length) {
             res = e;
         }
     })
@@ -528,20 +528,20 @@ const seperator = /,|、|&|＆/;
 
 function parse(str) {
     if (!str || localCache[str] === "NO_EXIST") {
-      return null;
+        return null;
     }
 
-    if(localCache[str]){
+    if (localCache[str]) {
         return localCache[str];
     }
 
-    const bMacthes =  match(bReg, str); //[]
+    const bMacthes = match(bReg, str); //[]
     const pMacthes = match(pReg, str);  //()
 
     const hasB = (bMacthes && bMacthes.length > 0);
     const hasP = (pMacthes && pMacthes.length > 0);
 
-    if(!hasB && !hasP){
+    if (!hasB && !hasP) {
         localCache[str] = "NO_EXIST";
         return;
     }
@@ -554,18 +554,18 @@ function parse(str) {
     let type;
     let tags = [];
 
-    function isOtherInfo(token){
+    function isOtherInfo(token) {
         let result = false;
-        if(isBookType(token)){
+        if (isBookType(token)) {
             type = getBookType(token);
             result = true;
-        }else if(isUselessTag(token)){
+        } else if (isUselessTag(token)) {
             //nothing
             result = true;
-        } else if(belongToEvent(token)){
+        } else if (belongToEvent(token)) {
             comiket = token;
             result = true;
-        }else if (isStrDate(token)) {
+        } else if (isStrDate(token)) {
             dateTag = token;  //e.g 190214
             result = true;
         }
@@ -577,28 +577,28 @@ function parse(str) {
         for (let ii = 0; ii < bMacthes.length; ii++) {
             let token = bMacthes[ii].trim();
             const tt = token.toLowerCase();
-            const nextCharIndex = str.indexOf(bMacthes[ii]) + bMacthes[ii].length + 1; 
+            const nextCharIndex = str.indexOf(bMacthes[ii]) + bMacthes[ii].length + 1;
             const nextChar = str[nextCharIndex];
 
-            if(isOtherInfo(token)){
+            if (isOtherInfo(token)) {
                 continue;
-            }  if (isNotAuthor(tt)){
+            } if (isNotAuthor(tt)) {
                 //e.g pixiv is not author
                 tags.push(token);
-            } else if (nextChar === "." || nextCharIndex >= str.length){
+            } else if (nextChar === "." || nextCharIndex >= str.length) {
                 //e.g KI-RecenT SP02 NATURALCORDE [DL版].zip
                 // [DL版] is not auhor name
                 tags.push(token);
-            } else if(!author) {
+            } else if (!author) {
                 //  [真珠貝(武田弘光)]
                 const temp = getGroupAndName(token);
-                if(temp.name && !isNotAuthor(temp.name)){
+                if (temp.name && !isNotAuthor(temp.name)) {
                     //e.g よろず is not author
                     author = temp.name;
-                    authors = author.split(seperator).map(e => e.trim()) ;
+                    authors = author.split(seperator).map(e => e.trim());
                 }
                 group = temp.group;
-            }else{
+            } else {
                 tags.push(token);
             }
         }
@@ -621,47 +621,47 @@ function parse(str) {
 
     tags = tags.map(e => e.trim());
 
-    tags = tags.filter(e=> {
-        return  e.length > 1 && !isOtherInfo(e) && authors.indexOf(e) === -1 && e !== author;
+    tags = tags.filter(e => {
+        return e.length > 1 && !isOtherInfo(e) && authors.indexOf(e) === -1 && e !== author;
     });
 
     tags = tags
-    .map(e => e.replace(/ |。/g, "").replace(/！/g, "!").replace(/？/g, "?"))
+        .map(e => e.replace(/ |。/g, "").replace(/！/g, "!").replace(/？/g, "?"))
 
     //tag reducing
     tags = tags.map(e => {
         const converts = [];
-        for(let ii = 0; ii < same_tag_matrix.length; ii++){
+        for (let ii = 0; ii < same_tag_matrix.length; ii++) {
             const row = same_tag_matrix[ii];
             const r = row[0];
-            if(e.match(r)){
+            if (e.match(r)) {
                 converts.push(row[1]);
             }
         }
 
-        if(converts.length > 0){
+        if (converts.length > 0) {
             return findMaxStr(converts);
-        }else{
+        } else {
             return e;
         }
     })
 
-    if(!type){
-        if(comiket|| group){
+    if (!type) {
+        if (comiket || group) {
             type = "Doujin";
-        }else{
+        } else {
             type = "etc";
         }
     }
 
-    if(!author && !group && tags.length === 0){
+    if (!author && !group && tags.length === 0) {
         localCache[str] = "NO_EXIST";
         return;
     }
 
     //get title
     let title = str;
-    (bMacthes||[]).concat( pMacthes||[], tags||[], [/\[/g, /\]/g, /\(/g, /\)/g ]).forEach(e => {
+    (bMacthes || []).concat(pMacthes || [], tags || [], [/\[/g, /\]/g, /\(/g, /\)/g]).forEach(e => {
         title = title.replace(e, "");
     })
     title = title.trim();
@@ -673,24 +673,24 @@ function parse(str) {
     })
 
     const result = {
-       dateTag, group, author, authors, tags, comiket, type, title
+        dateTag, group, author, authors, tags, comiket, type, title
     };
 
     localCache[str] = result;
     return result;
 }
 
-function parseMusicTitle(str){
+function parseMusicTitle(str) {
     // [161109] TVアニメ「ラブライブ！サンシャイン!!」挿入歌シングル3「想いよひとつになれ／MIRAI TICKET」／Aqours [320K].zip
     //[180727]TVアニメ『音楽少女』OPテーマ「永遠少年」／小倉唯[320K].rar
     let jpbReg = /「(.*?)」/g;
-    const macthes =  match(jpbReg, str) || [];
+    const macthes = match(jpbReg, str) || [];
 
     let jpbReg2 = /『(.*?)』/g;
-    const macthes2 =  match(jpbReg2, str) || [];
+    const macthes2 = match(jpbReg2, str) || [];
 
     return (macthes.concat(macthes2)).map(e => {
-       return e.trim();
+        return e.trim();
     })
 }
 
@@ -795,45 +795,45 @@ console.assert(editDistance("tozanbu", "tozan:bu") === 1);
 console.assert(editDistance("tozan；bu", "tozan:bu") === 1);
 //---------------------
 
-function compareInternalDigit(s1, s2){
+function compareInternalDigit(s1, s2) {
     const digitTokens1 = s1.match(/\d+/g);
     const digitTokens2 = s2.match(/\d+/g);
-    if(digitTokens1 && digitTokens2){
-        if(digitTokens1.length !== digitTokens2.length || 
-            digitTokens1.join() !== digitTokens2.join()){
+    if (digitTokens1 && digitTokens2) {
+        if (digitTokens1.length !== digitTokens2.length ||
+            digitTokens1.join() !== digitTokens2.join()) {
             return false;
         }
-    }else if(digitTokens1 && !digitTokens2){
+    } else if (digitTokens1 && !digitTokens2) {
         return false;
-    }else if(!digitTokens1 && digitTokens2){
+    } else if (!digitTokens1 && digitTokens2) {
         return false;
     }
     return true;
 }
 
-function isHighlySimilar(s1, s2){
-    if(!s1 && !s2){
+function isHighlySimilar(s1, s2) {
+    if (!s1 && !s2) {
         return true;
-    }else if(s1 && s2){
-        if(!compareInternalDigit(s1, s2)){
+    } else if (s1 && s2) {
+        if (!compareInternalDigit(s1, s2)) {
             return false;
         }
 
         const distance = editDistance(s1, s2);
-        const avgLen = (s1.length + s2.length)/2;
-        const ratio = distance/(Math.ceil(avgLen));
+        const avgLen = (s1.length + s2.length) / 2;
+        const ratio = distance / (Math.ceil(avgLen));
 
         return ratio <= 0.2;
-    }else{
+    } else {
         return false;
     }
 }
 
 console.assert(isHighlySimilar("tozanbu", "tozan:bu"))
 console.assert(isHighlySimilar("tobu", "to:bu"))
-console.assert(isHighlySimilar("12ab", "12abc")) 
+console.assert(isHighlySimilar("12ab", "12abc"))
 
-console.assert(isHighlySimilar("時雨露出×野外2", "白露型時雨露出×野外2") === false) ;
+console.assert(isHighlySimilar("時雨露出×野外2", "白露型時雨露出×野外2") === false);
 console.assert(isHighlySimilar("12a", "13a") === false);
 console.assert(isHighlySimilar("12", "ab") === false);
 
