864912bf0ad8d827ed8297f4986f08e65aec57f3
aji47 <the398355565@gmail.com>
2021-05-02 12:54:34 +0800

    update

--- src/server/routes/hentaiApi.js
+++ src/server/routes/hentaiApi.js
@@ -6,6 +6,26 @@ const db = require("../models/db");
 const util = global.requireUtil();
 const { isCompress } = util;
 
+// get to get all
+// http://localhost:8080/api/exhentaiApi
+router.get('/api/exhentaiApi', async (req, res) => {
+    let allFiles = db.getAllFilePathes().filter(isCompress);
+
+    const zipInfoDb = require("../models/zipInfoDb");
+    const zipInfo = zipInfoDb.getZipInfo(allFiles);
+
+    const result = {};
+    allFiles.forEach(e => {
+        const key = path.basename(e, path.extname(e)).trim();
+        result[key] = Object.assign({}, zipInfo[e]);
+    })
+
+    res.send({
+        allFiles: result
+    });
+})
+
+// post will take titleList parameter
 // http://localhost:8080/api/exhentaiApi
 router.post('/api/exhentaiApi', async (req, res) => {
     let allFiles = db.getAllFilePathes().filter(isCompress);
