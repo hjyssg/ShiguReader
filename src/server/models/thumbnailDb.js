5e5b81485444d3805a2eee21056c799ebbfcac93
aji47 <the398355565@gmail.com>
2021-10-06 16:09:31 +0800

    内存hash比sqlite块

--- src/server/models/thumbnailDb.js
+++ src/server/models/thumbnailDb.js
@@ -14,7 +14,7 @@ const rootPath = pathUtil.getRootPath();
 let thumbnail_db_path = path.join(rootPath, userConfig.workspace_name, "thumbnail_sql_db");
 const sqlite3 = require('sqlite3').verbose();
 const sqlDb = new sqlite3.Database(thumbnail_db_path);
-sqlDb.run("CREATE TABLE IF NOT EXISTS thumbnail_table (filePath TEXT, thumbnailFileName TEXT); CREATE INDEX IF NOT EXISTS filePath_index ON thumbnail_table (filePath)");
+sqlDb.run("CREATE TABLE IF NOT EXISTS thumbnail_table (filePath TEXT, thumbnailFileName TEXT); CREATE INDEX IF NOT EXISTS filePath_index ON thumbnail_table (filePath)", syncInterbalDict);
 
 const _util = require('util');
 sqlDb.allSync = _util.promisify(sqlDb.all).bind(sqlDb);
@@ -24,6 +24,8 @@ sqlDb.runSync = _util.promisify(sqlDb.run).bind(sqlDb);
 module.exports.addNewThumbnail = function (filePath, thumbnailFilePath) {
     const thumbnailFileName = path.basename(thumbnailFilePath);
     sqlDb.run("INSERT OR REPLACE INTO thumbnail_table(filePath, thumbnailFileName ) values(?, ?)", filePath, thumbnailFileName);
+
+    _internal_dict_[filePath] = {filePath, thumbnailFileName}
 }
 
 module.exports.init = function (filePathes) {
@@ -32,9 +34,7 @@ module.exports.init = function (filePathes) {
     //remove sql table
 
     //todo2: if the thumgbnail is deleted, but sql still keep the record
-
 }
-
 
 function _add_col(rows) {
     const thumbnailFolderPath = global.thumbnailFolderPath;
@@ -43,6 +43,17 @@ function _add_col(rows) {
     })
     return rows;
 }
+
+const _internal_dict_ = {};
+async function syncInterbalDict(){
+    const sql = `SELECT * FROM  thumbnail_table`;
+    let rows = await sqlDb.allSync(sql)
+
+    rows.forEach(e => {
+        _internal_dict_[e.filePath] = e;
+    })
+}
+
 
 //multiple
 module.exports.getThumbnailArr = async function (filePathes) {
@@ -53,12 +64,15 @@ module.exports.getThumbnailArr = async function (filePathes) {
     // const sql = `SELECT * FROM  thumbnail_table WHERE INSTR(?, filePath) > 0`;
     // let rows = await sqlDb.allSync(sql, [joinStr]);
 
-    const promiseArr = filePathes.map(fp => {
-        const sql = `SELECT * FROM  thumbnail_table WHERE filePath = ?`;
-        return sqlDb.getSync(sql, [fp]);
-    })
+    // const promiseArr = filePathes.map(fp => {
+    //     const sql = `SELECT * FROM  thumbnail_table WHERE filePath = ?`;
+    //     return sqlDb.getSync(sql, [fp]);
+    // })
 
-    let rows =  await Promise.all(promiseArr);
+    // let rows =  await Promise.all(promiseArr);
+    // rows = rows.filter(e => !!e);
+
+    let rows = filePathes.map(e => _internal_dict_[e]);
     rows = rows.filter(e => !!e);
     return _add_col(rows);
 }
