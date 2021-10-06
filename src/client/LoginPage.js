748475818cbe603f7ee6ef22563b984f1b42804b
aji47 <the398355565@gmail.com>
2021-10-06 17:47:42 +0800

    login

--- src/client/LoginPage.js
+++ src/client/LoginPage.js
@@ -13,6 +13,8 @@ import Sender from './Sender';
 
 // http://localhost:3000/
 class LoginPage extends Component {
+    state = {};
+
     getPasswordInput() {
         const pathInput = ReactDOM.findDOMNode(this.passwordInputRef);
         const text = (pathInput && pathInput.value) || "";
@@ -24,17 +26,20 @@ class LoginPage extends Component {
         Sender.post('/api/login', {"password":text}, res => {
             if (!res.isFailed()) {
                 window.location.href = "/"
+            }else{
+                this.setState({errMessage: "Wrong Password"});
             }
         });
     }
 
     render() {
         let content = (<React.Fragment>
-            <div className="admin-section-title">ShiguReader Login</div>
+            <div className="admin-section-title">ShiguReader</div>
             <div className="admin-section-content">
                 <input className="admin-intput" id="login-input" ref={pathInput => this.passwordInputRef = pathInput}
-                    placeholder="password here..." onChange={this.setPasswordCookie.bind(this)} />
-
+                    placeholder="password here..." onChange={()=> this.setState({errMessage: ""})}   type="password"/>
+                <button onClick={this.setPasswordCookie.bind(this)}> Login </button>
+                <div > {this.state.errMessage} </div>
                 <div className="author-link">
                     <a className="fab fa-github" title="Aji47's Github" href="https://github.com/hjyssg/ShiguReader" target="_blank"> Created By Aji47 </a>
                 </div>
