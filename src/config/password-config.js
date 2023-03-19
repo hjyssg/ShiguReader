48833702212ba8959ad1f32e933e7b79553ea681
aji47 <the398355565@gmail.com>
2023-03-19 14:11:27 +0800

    password

--- src/config/password-config.js
+++ src/config/password-config.js
@@ -5,7 +5,7 @@ const config = {};
 //global password
 //when set, user need to enter password to use ShiguReader
 //全局密码，设置以后用户必须输入密码才能打开网站
-config.home_password = "2021"
+config.home_password = (new Date()).getFullYear();
 
 //------------------------------------------------------------------
 module.exports = config;
