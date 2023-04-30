4c3faa4ece5c1e82d126da151ad0bc803af794ec
aji47 <the398355565@gmail.com>
2023-04-30 22:20:48 +0800

    [very important] download cache

--- src/server/routes/download.js
+++ src/server/routes/download.js
@@ -9,6 +9,7 @@ const { isImage, isGif } = util;
 
 const pathUtil = require("../pathUtil");
 const { isExist } = pathUtil;
+const memorycache = require('memory-cache');
 
 let sharp;
 try {
@@ -38,27 +39,37 @@ router.get('/api/download/', async (req, res) => {
 
     try {
         if (sharp && isImage(filePath) && !isGif(filePath)) {
-            const stat = await pfs.stat(filePath);
-            if (thumbnailMode && stat.size > THUMBNAIL_HUGE_THRESHOLD) {
-                const outputFn = stringHash(filePath).toString() + "-min.jpg";
-                const outputPath = path.resolve(global.cachePath, outputFn);
-                if (!(await isExist(outputPath))) {
-                    await sharp(filePath).resize({ height: 280 }).toFile(outputPath);
+            if(memorycache.get(filePath)){
+                filePath = memorycache.get(filePath);
+            }else{
+                const stat = await pfs.stat(filePath);
+                if (thumbnailMode && stat.size > THUMBNAIL_HUGE_THRESHOLD) {
+                    const outputFn = stringHash(filePath).toString() + "-min.jpg";
+                    const outputPath = path.resolve(global.cachePath, outputFn);
+                    if (!(await isExist(outputPath))) {
+                        await sharp(filePath).resize({ height: 280 }).toFile(outputPath);
+                    }
+                    memorycache.put(filePath, outputPath, 60*1000);
+                    filePath = outputPath;
+                }else if(stat.size > ONEBOOK_HUGE_THRESHOLD){
+                    const outputFn = stringHash(filePath).toString() + "-min-2.jpg";
+                    const outputPath = path.resolve(global.cachePath, outputFn);
+                    if (!(await isExist(outputPath))) {
+                        await sharp(filePath).resize({ height: 1980 }).toFile(outputPath);
+                    }
+                    memorycache.put(filePath, outputPath, 60*1000);
+                    filePath = outputPath;
                 }
-                filePath = outputPath;
-            }else if(stat.size > ONEBOOK_HUGE_THRESHOLD){
-                const outputFn = stringHash(filePath).toString() + "-min-2.jpg";
-                const outputPath = path.resolve(global.cachePath, outputFn);
-                if (!(await isExist(outputPath))) {
-                    await sharp(filePath).resize({ height: 1980 }).toFile(outputPath);
-                }
-                filePath = outputPath;
             }
         }
     } catch (e) {
         console.error("[file server error] during compression",e);
     }
 
+    // cache 1 hour
+    if(isImage(filePath)){
+        res.setHeader('Cache-Control', 'public, max-age=3600')
+    }
     res.download(filePath); // Set disposition and send it.
 });
 
