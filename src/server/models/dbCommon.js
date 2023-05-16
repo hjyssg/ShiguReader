634e83833e765702b643c0a1f2412677d0d8465d
aji47 <the398355565@gmail.com>
2023-05-16 12:50:24 +0800

    pkg真难，搞不定native addon

--- src/server/models/dbCommon.js
+++ src/server/models/dbCommon.js
@@ -2,16 +2,23 @@ const sql_lib = require('sqlite3');
 const _util = require('util');
 
 function getSQLInstance(filePath){
-    const sqlite3 = sql_lib.verbose();
-    const sqlDb = new sqlite3.Database(filePath);
+    // console.log("getSQLInstance" + filePath);
+    try{
+        const sqlite3 = sql_lib.verbose();
+        const sqlDb = new sqlite3.Database(filePath);
+    
+        //只用到下面这四个函数
+        sqlDb.allSync = _util.promisify(sqlDb.all).bind(sqlDb);
+        sqlDb.getSync = _util.promisify(sqlDb.get).bind(sqlDb);
+        sqlDb.runSync = _util.promisify(sqlDb.run).bind(sqlDb);
+        // run()
 
-    //只用到下面这四个函数
-    sqlDb.allSync = _util.promisify(sqlDb.all).bind(sqlDb);
-    sqlDb.getSync = _util.promisify(sqlDb.get).bind(sqlDb);
-    sqlDb.runSync = _util.promisify(sqlDb.run).bind(sqlDb);
-    // run()
-
-    return sqlDb;
+        return sqlDb;
+    }catch(e){
+        console.error("[SQLite] failed to open"+ filePath)
+        console.error(e);
+        throw e;
+    }
 }
 
 module.exports.getSQLInstance = getSQLInstance;
