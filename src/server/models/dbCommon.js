de7c84e0fd31b07398a15c229be54e284b0e9696
aji47 <the398355565@gmail.com>
2023-05-16 10:28:44 +0800

    comment

--- src/server/models/dbCommon.js
+++ src/server/models/dbCommon.js
@@ -5,9 +5,11 @@ function getSQLInstance(filePath){
     const sqlite3 = sql_lib.verbose();
     const sqlDb = new sqlite3.Database(filePath);
 
+    //只用到下面这四个函数
     sqlDb.allSync = _util.promisify(sqlDb.all).bind(sqlDb);
     sqlDb.getSync = _util.promisify(sqlDb.get).bind(sqlDb);
     sqlDb.runSync = _util.promisify(sqlDb.run).bind(sqlDb);
+    // run()
 
     return sqlDb;
 }
