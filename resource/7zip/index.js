e019a893796fb2d3a6032e514d0c50ad6f150e7d
aji47 <the398355565@gmail.com>
2023-05-20 19:11:44 +0800

    log

--- resource/7zip/index.js
+++ resource/7zip/index.js
@@ -2,7 +2,9 @@ var resolve = require('path').resolve
 var bin = require('./package').bin
 
 module.exports = map_obj(bin, function(v){
-  return resolve(__dirname, v)
+  let result = resolve(__dirname, v);
+  console.log("[DEBUG] 7Zpath", result);
+  return result;
 })
 
 function map_obj(obj, fn){
