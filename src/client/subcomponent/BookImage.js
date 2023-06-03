998010d17d9e778809f26f3bfce46d17910d67a1
aji47 <the398355565@gmail.com>
2023-06-03 16:57:23 +0800

    小修正

--- src/client/subcomponent/BookImage.js
+++ src/client/subcomponent/BookImage.js
@@ -6,31 +6,43 @@ import { useThrottleCallback } from "@react-hook/throttle";
 function BookImage(props, ref) {
   const { className, imageFiles, index, onLoad, onError, ...rest } = props;
   const [imageSrc, setImageSrc] = useState("");
+  const [prealoadMade, setprealoadMade] = useState({});
+
+  //   react hook在闭包拿到最新的props
+  const latestProps = useRef(props);
+  useEffect(() => {
+    // 当useEffect hook被调用时，它会在每次组件渲染后都执行一次。如果您没有传递第二个参数（依赖项数组），则useEffect hook将在每次组件渲染后都执行一次，并且没有任何限制。
+    latestProps.current = props;
+  });
 
   useEffect(() => {
     // 组件挂载时获取图片数据
     throttledGetImageData();
   }, [index]);
 
-  const doPreload =  async () => {
-    if(document.visibilityState === 'visible'){
+  const doPreload = async () => {
+    if (document.visibilityState === "visible") {
       const beg = index + 1;
       const preload_num = 2;
       const end = Math.min(beg + preload_num, imageFiles.length);
-  
-      for(let ii = beg; ii < end; ii++){
-        // const temp =  <link rel="preload" key={ii} href={getFileUrl(imageFiles[ii])} as="image" />
-        // result.push(temp);
+
+      for (let ii = beg; ii < end; ii++) {
         const url = clientUtil.getFileUrl(imageFiles[ii]);
+        // 避免重复load
+        if(prealoadMade[url]){
+            continue;
+        }
+        prealoadMade[url] = true;
+
         const response = await fetch(url, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/octet-stream",
-            },
-          });
+          method: "GET",
+          headers: {
+            "Content-Type": "application/octet-stream",
+          },
+        });
       }
     }
-  }
+  };
 
   async function getImageData() {
     try {
@@ -47,20 +59,28 @@ function BookImage(props, ref) {
           "Content-Type": "application/octet-stream",
         },
       });
+      prealoadMade[url] = true;
 
       const blob = await response.blob();
       if (blob.size > 0) {
         const reader = new FileReader();
         reader.onloadend = () => {
           const binaryString = reader.result;
-          setImageSrc(`data:image/jpeg;base64,${window.btoa(binaryString)}`);
+
+          // 闭包的坑
+          if (latestProps.current.index == index) {
+            setImageSrc(`data:image/jpeg;base64,${window.btoa(binaryString)}`);
+          } else {
+            console.log(latestProps.current.index, index);
+          }
+
+          onLoad && onLoad();
         };
         reader.readAsBinaryString(blob);
       }
 
       // preload
       doPreload();
-
     } catch (error) {
       console.error(error);
       console.error(error);
@@ -68,17 +88,16 @@ function BookImage(props, ref) {
     }
   }
 
-  
   const onImageError = (error) => {
     // https://stackoverflow.com/questions/5559578/having-links-relative-to-root
-    if(imageSrc){
-        console.error(error);
-        setImageSrc("/error_loading.png");
-        onError && onError()
+    if (imageSrc) {
+      console.error(error);
+      setImageSrc("/error_loading.png");
+      onError && onError();
     }
-  }
+  };
 
-  const throttledGetImageData = useThrottleCallback(getImageData, 4);
+  const throttledGetImageData = useThrottleCallback(getImageData, 10);
 
   return (
     <img
