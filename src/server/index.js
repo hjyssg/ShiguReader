d6648a268356d7339088d71eb1f021fdf074ea37
aji47 <the398355565@gmail.com>
2023-06-15 13:58:20 +0800

    [get]small fixing

--- src/server/index.js
+++ src/server/index.js
@@ -612,11 +612,12 @@ async function getThumbnailForFolders(filePathes) {
             const stringsToMatch = nextFilePathes; // string array of values
             const patterns = stringsToMatch.map(str => `${str}%`);
             const placeholders = patterns.map(() => 'filePath LIKE ?').join(' OR ');
-            const sql = `SELECT filePath FROM file_table WHERE isImage=true AND ${placeholders} `;
+            const sql = `SELECT filePath, isImage FROM file_table WHERE isImage=true AND (${placeholders}) `;
             let imagerows = await db.doSmartAllSync(sql, patterns);
             nextFilePathes.forEach(filePath => {
                 const findRow = findOne(imagerows, filePath);
                 if (findRow) {
+                    console.assert(findRow.isImage);
                     result[filePath] = findRow.filePath;
                 }
             })
