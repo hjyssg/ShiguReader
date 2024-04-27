e761d25ba7d1b5c0df33e16f707d12fe84c54e1c
hjy <the398355565@gmail.com>
2024-04-27 12:36:20 +0800

    update doc

--- etc/pkg_zip_tool.py
+++ etc/pkg_zip_tool.py
@@ -6,7 +6,7 @@ from datetime import date
 # 让chatgpt帮我写的打包脚本
 
 # The path to the project directory that will be compressed
-project_dir = "X:\\git\\SGR_pkg"
+project_dir = "F:\\git\\SGR_pkg"
 
 # The relative paths of the directories to be excluded from the compression
 dirs_to_exclude = ["thumbnails", "workspace", "cache"]
@@ -14,7 +14,7 @@ dirs_to_exclude = ["thumbnails", "workspace", "cache"]
 today = date.today().strftime("%Y-%m-%d")
 
 # Create a zipfile with the name project_files.zip and add each file and folder to it (excluding specified directories)
-zip_file_path = os.path.join("X:\\git", f"ShiguReader_{today}.zip")
+zip_file_path = os.path.join("F:\\git", f"ShiguReader_{today}.zip")
 with zipfile.ZipFile(zip_file_path, "w") as zipf:
     for root, dirs, files in os.walk(project_dir):
         # Exclude specified directories from being added to the zipfile
