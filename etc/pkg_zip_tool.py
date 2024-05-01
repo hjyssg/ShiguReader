3bce1215325e4f0abdb59855b80e3bdb7936b115
hjy <the398355565@gmail.com>
2024-05-01 12:42:44 +0800

    update pkg script

--- etc/pkg_zip_tool.py
+++ etc/pkg_zip_tool.py
@@ -6,23 +6,28 @@ from datetime import date
 # 让chatgpt帮我写的打包脚本
 
 # The path to the project directory that will be compressed
-project_dir = "F:\\git\\SGR_pkg"
+import os
+import shutil
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+root_folder = os.path.join(script_dir, '..\\..')
+pkg_folder =  os.path.join(root_folder, "SGR_pkg")   
 
 # The relative paths of the directories to be excluded from the compression
-dirs_to_exclude = ["thumbnails", "workspace", "cache"]
+dirs_to_exclude = ["thumbnails", "workspace", "cache", ".git"]
 
 today = date.today().strftime("%Y-%m-%d")
 
 # Create a zipfile with the name project_files.zip and add each file and folder to it (excluding specified directories)
-zip_file_path = os.path.join("F:\\git", f"ShiguReader_{today}.zip")
+zip_file_path = os.path.join(root_folder, f"ShiguReader_{today}.zip")
 with zipfile.ZipFile(zip_file_path, "w") as zipf:
-    for root, dirs, files in os.walk(project_dir):
+    for root, dirs, files in os.walk(pkg_folder):
         # Exclude specified directories from being added to the zipfile
         dirs[:] = [dd for dd in dirs if dd not in dirs_to_exclude]
         
         for file in files:
             abs_path = os.path.join(root, file)
-            rel_path = os.path.relpath(abs_path, project_dir)
+            rel_path = os.path.relpath(abs_path, pkg_folder)
             zipf.write(abs_path, arcname=rel_path)
 
 print("Done!")
