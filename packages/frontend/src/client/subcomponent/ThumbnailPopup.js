dbf70c4360d6450098de04d79cfac14225a5fcc2
hjy <the398355565@gmail.com>
2025-09-25 18:30:47 +0800

    Refine thumbnail popup styling and positioning

--- packages/frontend/src/client/subcomponent/ThumbnailPopup.js
+++ packages/frontend/src/client/subcomponent/ThumbnailPopup.js
@@ -16,8 +16,17 @@ class ThumbnailPopup extends Component {
         super(prop);
         this.url = prop.url;
         this.isHovering = false;
-        this.state = {};
+        this.state = {
+            popupPosition: null,
+            rerenderTick: false
+        };
         this.useVideoPreviewForFolder = false;
+        this.popupRef = React.createRef();
+        this.popupDimensions = {
+            width: 240,
+            height: 220
+        };
+        this.lastMousePosition = null;
 
         // a throttled function that can only call the func parameter maximally once per every wait milliseconds. 
         this.throttleGet = _.throttle(()=> {
@@ -31,12 +40,23 @@ class ThumbnailPopup extends Component {
             url: this.props.url
           });
         }
+        if (this.popupRef.current && this.isHovering) {
+            const rect = this.popupRef.current.getBoundingClientRect();
+            const width = Math.round(rect.width);
+            const height = Math.round(rect.height);
+            if (width && height && (width !== this.popupDimensions.width || height !== this.popupDimensions.height)) {
+                this.popupDimensions = { width, height };
+                if (this.lastMousePosition) {
+                    this.updatePopupPosition(this.lastMousePosition.x, this.lastMousePosition.y, false);
+                }
+            }
+        }
       }
 
     askRerender(){
-        this.setState({
-            rerenderTick: !this.state.rerenderTick
-        })
+        this.setState((prevState) => ({
+            rerenderTick: !prevState.rerenderTick
+        }))
     }
 
     async fetchData ()  {
@@ -59,14 +79,55 @@ class ThumbnailPopup extends Component {
         } 
     }
 
-    onMouseMove(){
+    updatePopupPosition(clientX, clientY, rememberMouse = true) {
+        if (rememberMouse) {
+            this.lastMousePosition = { x: clientX, y: clientY };
+        }
+        const offset = 12;
+        const contentWidth = this.popupDimensions.width || 240;
+        const contentHeight = this.popupDimensions.height || 220;
+        let left = clientX - contentWidth - offset;
+        if(left < offset){
+            left = clientX + offset;
+        }
+        let top = clientY - contentHeight / 2;
+        const minTop = offset;
+        const maxTop = window.innerHeight - contentHeight - offset;
+        if(top < minTop){
+            top = minTop;
+        }
+        if(top > maxTop){
+            top = maxTop;
+        }
+        const { popupPosition } = this.state;
+        if (popupPosition && popupPosition.top === top && popupPosition.left === left) {
+            return;
+        }
+        this.setState({
+            popupPosition: {
+                top,
+                left
+            }
+        });
+    }
+
+    onMouseMove(e){
         this.isHovering = true;
         this.throttleGet();
-        this.askRerender();
+        if(!e){
+            this.askRerender();
+            return;
+        }
+        const { clientX, clientY } = e;
+        this.updatePopupPosition(clientX, clientY);
     }
 
     onMouseOut(){
         this.isHovering = false;
+        this.setState({
+            popupPosition: null
+        });
+        this.lastMousePosition = null;
         this.askRerender();
     }
 
@@ -80,6 +141,11 @@ class ThumbnailPopup extends Component {
         let extraDom = null;
         let titleStr = clientUtil.getBaseName(filePath);
         titleStr = util.truncateString(titleStr, 35);
+        const popupStyle = this.state.popupPosition ? {
+            top: this.state.popupPosition.top,
+            left: this.state.popupPosition.left
+        } : null;
+
         if(this.isHovering){
             if(isVideo(filePath)|| (this.useVideoPreviewForFolder && this.url)){
                 let src;
@@ -89,7 +155,7 @@ class ThumbnailPopup extends Component {
                     src = this.url;
                 }
 
-                extraDom = (<div className='thumbnail-popup-content'>
+                extraDom = (<div className='thumbnail-popup-content' style={popupStyle} ref={this.popupRef}>
                 <div className='thumbnail-popup-title'>{titleStr}</div>
                     <video className={"thumbnail-video-preview"} src={src} autoPlay={true} muted>
                         Your browser does not support the video tag.
@@ -97,12 +163,12 @@ class ThumbnailPopup extends Component {
                 </div>)
 
             } else if(url){
-                extraDom = (<div className='thumbnail-popup-content'>
+                extraDom = (<div className='thumbnail-popup-content' style={popupStyle} ref={this.popupRef}>
                 <div className='thumbnail-popup-title'>{titleStr}</div>
                 <img className='thumbnail-popup-img' src={url}></img>
                 </div>)
             }else{
-                extraDom = (<div className='thumbnail-popup-content'>
+                extraDom = (<div className='thumbnail-popup-content' style={popupStyle} ref={this.popupRef}>
                 <div className='thumbnail-popup-title'>{titleStr}</div>
                 <div className='thumbnail-popup-text'>NO_THUMBNAIL_AVAILABLE</div>
              </div>)
