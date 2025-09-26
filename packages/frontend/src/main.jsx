a4a73b4644703c463362f8fff9cd980d9093b3c3
hjy <the398355565@gmail.com>
2025-09-26 19:07:35 +0800

    Use npm Font Awesome package

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import '@styles/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
