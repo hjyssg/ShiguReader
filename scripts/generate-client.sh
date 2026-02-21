da4ba99f1deb178cdafe53bbbc9b77e361604ba6
hjy <email>
2026-02-22 01:57:08 +0800

    chore: remove Python backend leftovers (requirements, pyproject, uv.lock, copier, scripts)

--- scripts/generate-client.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# NOTE: Python backend has been removed. This script is no longer used.
-# The frontend client code in frontend/src/client/ is now maintained manually
-# or can be regenerated if a Node.js OpenAPI endpoint is added to backendnode.
-echo "generate-client.sh: Python backend removed, script disabled."
-exit 0
